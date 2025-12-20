501cb7c77e767f0027e46a81d26fe57acf0c1cd4
wexlerdev <alexwexlerdev@gmail.com>
2025-12-20 10:44:27 -0600

    Git Workflow Commit 1
    this is the description for a commit
    that tests git's professional workflow
    that i will use at some point in my career

--- web/src/app/host/HostDashboardClient.tsx
+++ web/src/app/host/HostDashboardClient.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+// Host dashboard client component
+
 import { useEffect, useMemo, useState } from "react";
 import { useRouter } from "next/navigation";
 import { QRCodeCanvas } from "qrcode.react";
